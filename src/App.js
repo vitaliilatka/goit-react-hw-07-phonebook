4f0a3133a58a578bdda1f5efc69ed08575cbd53c
vetallatka <latka.vitalik@yandex.ru>
2021-05-01 14:21:24 +0200

    added App.js && index.js

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import Container from './components/Container/Container';
+import ContactForm from './components/ContactForm/ContactForm';
+import ContactList from './components/ContactList/ContactList';
+import Filter from './components/Filter/Filter';
+import Spinner from './components/Loader/Loader';
+import { connect } from 'react-redux';
+import { useEffect } from 'react';
+import { contactsOperations, contactsSelectors } from './redux/contacts';
 
-function App() {
+const App = ({ isLoading, isError, fetchContacts }) => {
+  // eslint-disable-next-line
+  useEffect(() => fetchContacts(), []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-      </header>
-    </div>
+    <Container>
+      <h1>Phonebook</h1>
+      <ContactForm />
+      <h2>Contacts</h2>
+      <Filter />
+      {isLoading ? (
+        <Spinner />
+      ) : isError ? (
+        <p>sorry, there is an error :(</p>
+      ) : (
+        <ContactList />
+      )}
+    </Container>
   );
-}
+};
 
-export default App;
+const mapStateToProps = state => ({
+  isLoading: contactsSelectors.getLoading(state),
+  isError: contactsSelectors.getError(state),
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetchContacts: () => dispatch(contactsOperations.fetchContacts()),
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
